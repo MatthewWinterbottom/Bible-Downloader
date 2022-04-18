b8c509463d7db3a0dbcedd66d72f4044742f5795
Matthew <matthewwinterbottom@outlook.com>
2022-04-18 09:17:28 +0100

    Initial commit

---
+++ BibleDownloader/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+using var client = new HttpClient();
+
+var url = "https://biblehub.com/niv/matthew/13.htm";
+
+var webHtml = await client.GetStringAsync(url);
+
+Console.WriteLine(webHtml);
