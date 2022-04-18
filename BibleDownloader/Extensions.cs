bc7c7dd84b107409da385649d8a179be4b7c6102
Matthew <matthewwinterbottom@outlook.com>
2022-04-18 09:54:13 +0100

    progress...

---
+++ BibleDownloader/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using AngleSharp.Dom;
+
+namespace BibleDownloader;
+
+public static class Extensions
+{
+    /// <summary>
+    /// Gets the text of the immediate children
+    /// </summary>
+    /// <returns></returns>
+    public static string GetTextJoined(this IElement element, string? joiner = null)
+    {
+        joiner ??= Environment.NewLine;
+        
+        var textStrings = element.ChildNodes.OfType<IText>().Select(x => x.Text);
+        
+        return string.Join(joiner, textStrings);
+    }
+
+    public static string GetChildText(IElement html, string existingText = "")
+    {
+        var textWithinCurrentNode = html.GetTextJoined();
+
+        existingText += textWithinCurrentNode;
+        
+        // Now we need to look at text within children
+
+        foreach (var child in html.Children)
+        {
+            GetChildText(child, existingText);
+        }
+
+        return existingText;
+    }
+}
--- BibleDownloader/Program.cs
+++ BibleDownloader/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.RegularExpressions;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+
 Console.WriteLine("Hello, World!");
 
 using var client = new HttpClient();
@@ -9,3 +13,27 @@ var url = "https://biblehub.com/niv/matthew/13.htm";
 var webHtml = await client.GetStringAsync(url);
 
 Console.WriteLine(webHtml);
+
+var parser = new HtmlParser();
+
+var document = parser.ParseDocument(webHtml);
+
+var regions = document.QuerySelectorAll(".reg");
+
+foreach (var region in regions)
+{
+    var children = region.Children;
+    
+    
+}
+
+var textFromRegions = regions.SelectMany(x => x.ChildNodes.OfType<IText>().Select(x => x.Text));
+
+var textTogether = string.Join(Environment.NewLine, textFromRegions);
+
+Console.WriteLine(textTogether);
+
+
+
+
+
